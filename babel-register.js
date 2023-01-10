6fa8111c193bccf62d515e4dbef69f6b950971d2
Goncharovn Nikita <n.goncharov@factor-ts.ru>
2023-01-10 17:14:35 +0300

    fix bugs

--- babel-register.js
+++ babel-register.js
@@ -1,16 +1,15 @@
 const register = require('@babel/register').default;
-const {JSDOM} = require("jsdom");
+const { JSDOM } = require('jsdom');
 
 register({
-    presets: [
-        '@babel/preset-typescript',
-        '@babel/preset-env'
-    ],
-    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  presets: [
+    '@babel/preset-typescript',
+    '@babel/preset-env',
+  ],
+  extensions: ['.ts', '.tsx', '.js', '.jsx'],
 });
 
-
-const {window} = new JSDOM('<div id="app"></div>', {url: 'https://localhost'});
+const { window } = new JSDOM('<div id="app"></div>', { url: 'https://localhost' });
 
 global.window = window;
 global.document = window.document;
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const DuplicatePackageCheckerPlugin = require("duplicate-package-checker-webpack-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const DuplicatePackageCheckerPlugin = require('duplicate-package-checker-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
-const isDev = process.env.NODE_ENV === "development";
+const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.ts",
-  devtool: "inline-source-map",
+  mode: 'development',
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
   output: {
-    path: path.resolve(__dirname, "./dist"),
-    filename: isDev ? "[name].[contenthash].js" : "[name].js",
+    path: path.resolve(__dirname, './dist'),
+    filename: isDev ? '[name].[contenthash].js' : '[name].js',
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js", ".json"],
+    extensions: ['.tsx', '.ts', '.js', '.json'],
     alias: {
-      handlebars: "handlebars/dist/handlebars.min.js"
-    }
+      handlebars: 'handlebars/dist/handlebars.min.js',
+    },
   },
   devServer: {
     compress: true,
@@ -31,33 +31,33 @@ module.exports = {
     rules: [
       {
         test: /\.ts?$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
       {
         test: /\.hbs$/,
-        use: ["handlebars-loader"],
+        use: ['handlebars-loader'],
         exclude: /(node_modules)/,
       },
       {
         test: /\.scss$/i,
         use: [
           // compiles Less to CSS
-          "style-loader",
-          "css-loader",
-          "sass-loader",
+          'style-loader',
+          'css-loader',
+          'sass-loader',
         ],
       },
       // images
-      { test: /\.svg$/, type: "asset" },
+      { test: /\.svg$/, type: 'asset' },
       {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              name: "[name].[ext]",
+              name: '[name].[ext]',
               useRelativePath: true,
               esModule: false,
             },
@@ -67,22 +67,22 @@ module.exports = {
       // fonts
       {
         test: /\.(woff(2)?|eot|ttf|otf)$/,
-        type: "asset/inline",
+        type: 'asset/inline',
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: isDev ? "[name].[contenthash].scss" : "[name].scss",
+      filename: isDev ? '[name].[contenthash].scss' : '[name].scss',
     }),
-    new DuplicatePackageCheckerPlugin()
+    new DuplicatePackageCheckerPlugin(),
   ],
   optimization: {
     minimizer: [
       new CssMinimizerPlugin(),
-    ]
-  }
+    ],
+  },
 };
